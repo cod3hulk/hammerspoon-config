2df2dba61d9369514e0e4ca30e9a542456a80ea3
Tomas Ave <cod3hulk@gmail.com>
2017-05-09 10:11:37 +0200

    Add window management keybindings

--- Spoons/Window.spoon/init.lua
+++ Spoons/Window.spoon/init.lua
@@ -1,6 +1,39 @@
 local mod = {}
 
+hs.grid.setGrid('4x4')
+
+local gridset = function(x, y, w, h)
+    return function()
+        local window = hs.window.focusedWindow()
+        hs.grid.set(
+            window,
+            {x=x, y=y, w=w, h=h},
+            window:screen()
+        )
+    end
+end
+
+
+local hyper = {"ctrl", "cmd"}
+
 function mod.bindKeys()
+    hs.hotkey.bind(hyper, "k", function()
+        local window = hs.window.focusedWindow()
+        local grid = hs.grid.get(window)
+        if grid.x == 0.0 and grid.y == 0.0 and grid.w == 4 and grid.h == 4 then
+            local screen = window:screen()
+            if screen:toEast() then
+                window:moveOneScreenEast()
+            else
+                window:moveOneScreenWest()
+            end
+        end
+        hs.grid.maximizeWindow(window)
+    end)
+    -- Left 1/2 of the screen
+    hs.hotkey.bind(hyper, "h", gridset(0, 0, 2, 4))
+    -- Right 1/2 of the screen
+    hs.hotkey.bind(hyper, "l", gridset(2, 0, 2, 4))
 end
 
 return mod
