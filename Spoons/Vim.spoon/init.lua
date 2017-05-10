4778b1b079fd40bb7f4037df502edb432f14d2d7
Tomas Ave <cod3hulk@gmail.com>
2017-05-10 11:12:09 +0200

    Simplfy keybindings

--- Spoons/Vim.spoon/init.lua
+++ Spoons/Vim.spoon/init.lua
@@ -1,53 +1,20 @@
 local event = require("hs.eventtap").event
 local mod = {}
 
-function pressKey(modifiers, character)
-    event.newKeyEvent(modifiers, string.lower(character), true):post()
-    event.newKeyEvent(modifiers, string.lower(character), false):post()
-end
-
--- VI KEYS
-function right() 
-    pressKey({}, "Right") 
-end
-
-function left() 
-    pressKey({}, "Left") 
-end
-
-function up() 
-    pressKey({}, "Up") 
-end
-
-function down() 
-    pressKey({}, "Down") 
-end
-
-function endWord() 
-    pressKey({"alt"}, "Right") 
-end
-
-function beginWord() 
-    pressKey({"alt"}, "Left") 
-end
-
-function moveToEOL()
-    pressKey({"cmd"}, "Right")
-end
-
-function moveToSOL()
-    pressKey({"cmd"}, "Left")
+local pressKey = function(modifiers, character)
+    return function()
+        event.newKeyEvent(modifiers, string.lower(character), true):post()
+        event.newKeyEvent(modifiers, string.lower(character), false):post()
+    end
 end
 
 function mod.bindKeys(modal)
-    modal:bind({}, 'h', left)
-    modal:bind({}, 'l', right)
-    modal:bind({}, 'k', up)
-    modal:bind({}, 'j', down)
-    modal:bind({}, 'e', endWord)
-    modal:bind({}, 'b', beginWord)
-    modal:bind({"shift"}, '4', moveToEOL)
-    modal:bind({}, '0', moveToSOL)
+    modal:bind({}, 'h', pressKey({}, "Left"))
+    modal:bind({}, 'l', pressKey({}, "Right"))
+    modal:bind({}, 'k', pressKey({}, "Up"))
+    modal:bind({}, 'j', pressKey({}, "Down"))
+    modal:bind({}, 'e', pressKey({"alt"}, "Right"))
+    modal:bind({}, 'b', pressKey({"alt"}, "Left"))
 end
 
 return mod
